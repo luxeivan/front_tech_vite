7c04db1147e47e5b7ebd80e17258515d91ff8a9d
Stanislav <yanut995@gmail.com>
2025-08-26 15:40:48 +0300

    Фикс. Поправил хэр ещё раз с другим лого

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,24 @@
 import { Button, Flex, Image } from "antd";
 import React from "react";
 import useAuth from "../stores/useAuth";
-import logo from "../img/logo.svg";
+import logo from "../img/logoBlue.svg";
 
 export default function Header() {
-  const { authing, isAuth, exit } = useAuth((store) => store);
+  const { isAuth, exit } = useAuth((store) => store);
 
   return (
     <Flex
       justify="space-between"
       align="center"
-      style={{ padding: 20, backgroundColor: "#0061aa" }}
+      style={{
+        padding: "12px 20px",
+        backgroundColor: "#fff", // белый фон вместо синего
+        borderBottom: "1px solid #eaeaea", // тонкая сероватая линия снизу для аккуратности
+      }}
     >
-      <Image src={logo} preview={false} />
+      <Image src={logo} preview={false} height={40} />
       {isAuth && (
-        <Button
-          type="primary"
-          danger
-          onClick={() => {
-            exit();
-          }}
-        >
+        <Button type="primary" danger onClick={() => exit()}>
           Выйти
         </Button>
       )}
